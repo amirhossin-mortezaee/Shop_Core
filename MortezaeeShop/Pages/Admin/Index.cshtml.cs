567d808c08b3932d5b7436e0ddbc24ac3a8742ce
amirhossin-mortezaee <mortezaeeamirhoussin@gmail.com>
2024-09-03 17:20:57 +0330

    ساخت پنل مدیریت محصولات با Razor Pages

--- MortezaeeShop/Pages/Admin/Index.cshtml.cs
+++ MortezaeeShop/Pages/Admin/Index.cshtml.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MortezaeeShop.Data;
+using MortezaeeShop.Models;
 
 namespace MortezaeeShop.Pages.Admin
 {
     public class IndexModel : PageModel
     {
+        private MortezaeeShopContext _context;
+        public IndexModel(MortezaeeShopContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Product> Products { get; set; }
         public void OnGet()
+        {
+            Products = _context.Products.Include(p => p.Item);
+        }
+
+        public void OnPost()
         {
         }
     }
