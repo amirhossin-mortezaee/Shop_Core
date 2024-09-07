43001ec3845ce6f7cfb370d0f8d133b1bd1c4751
amirhossin-mortezaee <mortezaeeamirhoussin@gmail.com>
2024-09-07 13:58:03 +0330

    ساخت مدیریت کاربران با Scaffolding

---
+++ MortezaeeShop/Pages/Admin/ManageUsers/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MortezaeeShop.Models;
+
+namespace MyEshop.Pages.Admin.ManageUsers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MortezaeeShop.Data.MortezaeeShopContext _context;
+
+        public IndexModel(MortezaeeShop.Data.MortezaeeShopContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Users> Users { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Users = await _context.Users.ToListAsync();
+        }
+    }
+}
