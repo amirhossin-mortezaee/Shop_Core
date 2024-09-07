ec93171fb1fc06d29ba9ea43407397002bf9b342
amirhossin-mortezaee <129367158+amirhossin-mortezaee@users.noreply.github.com>
2024-09-07 10:54:32 +0330

    ساخت افزودن محصول و آپلود تصویر

---
+++ MortezaeeShop/Models/AddEditProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MortezaeeShop.Models
+{
+    public class AddEditProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Decimal Price { get; set; }
+        public int QuantityInStock { get; set; }
+        public IFormFile Picture { get; set; }
+    }
+}
--- MortezaeeShop/Pages/Admin/Add.cshtml.cs
+++ MortezaeeShop/Pages/Admin/Add.cshtml.cs
@@ -1,16 +1,67 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MortezaeeShop.Data;
+using MortezaeeShop.Models;
 
 namespace MortezaeeShop.Pages.Admin
 {
     public class AddModel : PageModel
     {
+        private MortezaeeShopContext _context;
+
+        public AddModel(MortezaeeShopContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public AddEditProductViewModel Product { get; set; }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            var item = new Item()
+            {
+                Price = Product.Price,
+                QuantityInStock = Product.QuantityInStock
+            };
+            _context.Add(item);
+            _context.SaveChanges();
+
+            var pro = new Product()
+            {
+                Name = Product.Name,
+                Item = item,
+                Discription = Product.Description
+            };
+            _context.Add(pro);
+            _context.SaveChanges();
+
+            pro.ItemId = pro.Id;
+            _context.SaveChanges();
+
+            if(Product.Picture?.Length > 0)
+            {
+                string FilePath = Path.Combine(Directory.GetCurrentDirectory(),
+                    "wwwroot",
+                    "images",
+                    pro.Id + Path.GetExtension(Product.Picture.FileName));
+                using (var stream = new FileStream(FilePath,FileMode.Create))
+                {
+                    Product.Picture.CopyTo(stream);
+                }
+            }
+            return RedirectToPage("Index");
         }
     }
 }
